d0802ff6028c55ddfd4793c835b476fcb4e175f6
Boris.k <Bulldogxxx@gmail.com>
2014-12-02 20:05:26 +0200

    test new git

---
+++ src/Classes/Card.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Classes;
+
+/**
+ *
+ * @author BK
+ */
+public class Card {
+
+   int suit;
+   int rank;
+   Boolean side;
+   String CardFaceIcon;
+   int value;
+
+    public Card(int suit, int rank, Boolean side, String CardFaceIcon, int value) {
+        this.suit = suit;
+        this.rank = rank;
+        this.side = side;
+        this.CardFaceIcon = CardFaceIcon;
+        this.value = value;
+    }
+    
+   
+    
+   
+           
+   
+    
+}
